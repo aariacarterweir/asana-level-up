0da4b82d531e80484b92b31147a8ee8ded998438
Aaria Carter-Weir <aaria.carterweir@gmail.com>
2017-08-23 10:15:43 +1000

    Fixing transparent bg

--- unsplashbg-asana.user.js
+++ unsplashbg-asana.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name        unsplashbg-asana
 // @description Background from unsplash.com as background in asana.
+// @author      Ariana Carter-Weir
 // @namespace   unsplashbg-asana
 // @include     https://app.asana.com/*
 // @version     1.0
@@ -33,25 +34,36 @@ unsplashbg.changeBg = function() {
         // create new style
         var DLstyle = document.createElement("style");
             DLstyle.setAttribute("id", "unsplashbg-style");
-            DLstyle.textContent = ' body, #bg_pattern { ' +
-                'background-image: url("'+ response.finalUrl +'") !important; ' +
-                'background-position: top center; ' +
-                'background-size: cover; ' +
-                'background-repeat: no-repeat; }' +
-
-                '.lunaui-grid-center-pane-container #center_pane, ' +
-                '.lunaui-grid-center-pane-container #right_pane, ' +
-                '#right_pane_container #center_pane, ' +
-                '#right_pane_container #right_pane, ' +
-                '.asana2View-taskPane ' +
+            DLstyle.textContent = ' body, #bg_pattern ' +
                 '{' +
-                    'background-color: rgba(255,255,255,0.955); '+
-                '}' +
-                '.SingleTaskTitleRow-taskName textarea {background: transparent !important;}';
+                    'background-image: url("'+ response.finalUrl +'") !important; ' +
+                    'background-position: top center; ' +
+                    'background-size: cover; ' +
+                    'background-repeat: no-repeat;' +
+                '}';
             document.head.appendChild(DLstyle);
       }
     });
 };
+
+
+// add generic styles to doc
+unsplashbg.basestyles = document.createElement("style");
+unsplashbg.basestyles.textContent = 'body .lunaui-grid-center-pane-container #center_pane, ' +
+                                    'body .lunaui-grid-center-pane-container #right_pane, ' +
+                                    'body #right_pane_container #center_pane, ' +
+                                    'body #right_pane_container #right_pane, ' +
+                                    '.asana2View-taskPane ' +
+                                    '{' +
+                                        'background-color: rgba(255,255,255,0.955); '+
+                                    '}' +
+
+                                    '.SingleTaskTitleRow-taskName textarea, ' +
+                                    '.Tokenizer' +
+                                    '{' +
+                                        'background: transparent !important;' +
+                                    '}';
+document.head.appendChild(unsplashbg.basestyles);
 
 // call the fn
 unsplashbg.changeBg();

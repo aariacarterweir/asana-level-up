fdd18953b7c01035ceae4a75b55ba79a9b2d5b07
Aaria Carter-Weir <aaria.carterweir@gmail.com>
2017-09-14 10:31:36 +1000

    Small performance fixes

--- unsplashbg-asana.user.js
+++ unsplashbg-asana.user.js
@@ -12,54 +12,55 @@
 // namespace
 var unsplashbg = {
     options: {
-        interval: 20 * 1000, // (seconds) * 1000
+        interval: 30 * 1000, // (seconds) * 1000
         size: '1920x1080',
         transitionDuration : '2s',
         path: 'user/aariacarterweir/likes' // no trailing slash pls
     }
 };
 
+unsplashbg.styles = [];
+
 // change bg fn
 unsplashbg.changeBg = function() {
     clearInterval(unsplashbg.timer);
-    
-    GM_xmlhttpRequest({
-      method: 'GET',
-      url: 'https://source.unsplash.com/' + unsplashbg.options.path + '/' + unsplashbg.options.size,
-      headers: {
-        'User-Agent': 'Mozilla/5.0', // If not specified, navigator.userAgent will be used.
-        'Accept': 'text/xml' // If not specified, browser defaults will be used.
-      },
-      onload: function (response) {
-        
-        // preload the actual image now
-        GM_xmlhttpRequest({
-            method: 'GET',
-            url: response.finalUrl,
-            onload: function(r){
-                // delete previous style
-                if (document.getElementById('unsplashbg-style')) {
-                    document.getElementById('unsplashbg-style').setAttribute('id', 'unsplash-style-old');
-                }
 
-                // create new style
-                var DLstyle = document.createElement("style");
-                    DLstyle.setAttribute("id", "unsplashbg-style");
+    GM_xmlhttpRequest({
+        method: 'GET',
+        url: 'https://source.unsplash.com/' + unsplashbg.options.path + '/' + unsplashbg.options.size,
+        headers: {
+            'User-Agent': 'Mozilla/5.0', // If not specified, navigator.userAgent will be used.
+            'Accept': 'text/xml' // If not specified, browser defaults will be used.
+        },
+        onload: function (response) {
+
+            // preload the actual image now
+            GM_xmlhttpRequest({
+                method: 'GET',
+                url: response.finalUrl,
+                onload: function(r){
+                    // create new style
+                    var DLstyle = document.createElement("style");
                     DLstyle.textContent = ' body, #bg_pattern ' +
                         '{' +
-                            'background-image: url("'+ response.finalUrl +'") !important; ' +
+                        'background-image: url("'+ response.finalUrl +'") !important; ' +
                         '}';
-                document.head.appendChild(DLstyle);
+                    document.head.appendChild(DLstyle);
+                    unsplashbg.styles.push(DLstyle);
 
-                if (document.getElementById('unsplashbg-style-old')) {
-                    document.getElementById('unsplashbg-style-old').outerHTML = '';
+                    var oldStyle;
+                    if ( (oldStyle = unsplashbg.styles[unsplashbg.styles.length - 2]) ) {
+                        // cleanup later...
+                        setTimeout(function(){
+                            oldStyle.outerHTML = '';
+                        }, 10000);
+                    }
+
+                    // kick off another timer
+                    unsplashbg.timer = setInterval(unsplashbg.changeBg, unsplashbg.options.interval);
                 }
-
-                // kick off another timer
-                unsplashbg.timer = setInterval(unsplashbg.changeBg, unsplashbg.options.interval);
-            }
-        });
-      }
+            });
+        }
     });
 };
 
@@ -67,30 +68,40 @@ unsplashbg.changeBg = function() {
 // add generic styles to doc
 unsplashbg.basestyles = document.createElement("style");
 unsplashbg.basestyles.textContent = 'body .lunaui-grid-center-pane-container #center_pane, ' +
-                                    'body .lunaui-grid-center-pane-container #right_pane, ' +
-                                    'body #right_pane_container #center_pane, ' +
-                                    'body #right_pane_container #right_pane, ' +
-                                    '.asana2View-taskPane ' +
-                                    '{' +
-                                        'background-color: rgba(255,255,255,0.955); '+
-                                    '}' +
+    'body .lunaui-grid-center-pane-container #right_pane, ' +
+    'body #right_pane_container #center_pane, ' +
+    'body #right_pane_container #right_pane, ' +
+    '.asana2View-taskPane ' +
+    '{' +
+    'background-color: rgba(255,255,255,0.955); '+
+    '}' +
 
-                                    'body, #bg_pattern {' +
-                                        'background-position: top center; ' +
-                                        'background-size: cover; ' +
-                                        'background-repeat: no-repeat;' +
-                                        '-webkit-transition: background-image ' + unsplashbg.options.transitionDuration + ' ease-in-out; ' +
-                                        'transition: background-image ' + unsplashbg.options.transitionDuration + ' ease-in-out; ' +
-                                    '}' +
+    'body, #bg_pattern { ' +
+    '-webkit-backface-visibility: hidden;   -moz-backface-visibility: hidden;   -ms-backface-visibility: hidden;' +
+    'backface-visibility: hidden;  ' +
+    'background-position: top center; ' +
+    'background-size: cover; ' +
+    'background-repeat: no-repeat;' +
+    '-webkit-transition: background-image ' + unsplashbg.options.transitionDuration + ' ease-in-out; ' +
+    'transition: background-image ' + unsplashbg.options.transitionDuration + ' ease-in-out; ' +
+    '}' +
 
-                                    '.Sidebar { background-color: rgba(34, 43, 55, 0.9) !important; }' +
+    '.Sidebar { background-color: rgba(34, 43, 55, 0.9) !important; }' +
 
-                                    '.SingleTaskTitleRow-taskName textarea, ' +
-                                    '.Tokenizer' +
-                                    '{' +
-                                        'background: transparent !important;' +
-                                    '}';
+    '.SingleTaskTitleRow-taskName textarea, ' +
+    '.Tokenizer' +
+    '{' +
+    'background: transparent !important;' +
+    '}';
 document.head.appendChild(unsplashbg.basestyles);
 
 // call the fn
 unsplashbg.changeBg();
+
+
+// register a key binding
+$(document).bind('keydown', function(e){
+    if(e.ctrlKey && e.shiftKey && e.keyCode === 190) {
+        unsplashbg.changeBg();
+    }
+});
